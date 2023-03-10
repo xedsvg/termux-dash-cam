894f33009aae8f0fdb02de0549920ffae8b4655a
xedsvg <vlad@redcodenetwork.ro>
2023-03-10 13:57:37 +0200

    fix: accelerometer mapping

--- actions.js
+++ actions.js
@@ -1,28 +1,33 @@
-const { BatteryStatus, accelerometer } = require('termux');
+const { BatteryStatus } = require('termux');
+const sensor = require('node-sensor').Sensor;
 
 class Actions {
   constructor() {
     this.subscriptions = new Map();
+    this.accelerometer = new sensor('accelerometer');
   }
 
   subscribe(event, callback) {
     this.subscriptions.set(event, callback);
   }
 
-  start() {
-    // Listen for bump events
-    accelerometer.addListener(({ x, y, z }) => {
-      // Calculate the magnitude of the acceleration vector
-      const acceleration = Math.sqrt(x ** 2 + y ** 2 + z ** 2);
-
-      // Check if there was a bump (i.e. acceleration exceeded a certain threshold)
-      if (acceleration > 10) {
+  handleAccelerometerData(data) {
+    // Calculate the magnitude of the acceleration vector
+    const acceleration = Math.sqrt(Math.pow(data.x, 2) + Math.pow(data.y, 2) + Math.pow(data.z, 2));
+    
+    // Check if there was a bump (i.e. acceleration exceeded a certain threshold)
+    if (acceleration > 10) {
         const callback = this.subscriptions.get('bump');
         if (callback) {
           callback();
         }
-      }
-    });
+    }
+  }
+
+  start() {
+    // Listen for bump events
+    this.accelerometer.start();
+    this.accelerometer.on('data', data => this.handleAccelerometerData(data));
 
     // Listen for battery events
     const batteryStatus = new BatteryStatus({ refreshInterval: 10000 });
